d897fa7a2cb964cc6398cce2a037d593aadc8a3c
fukayatsu <fukayatsu@gmail.com>
2014-05-31 23:40:01 +0900

    Fix hook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :basic_auth
+  before_action :basic_auth
 
 private
 
--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,4 +1,7 @@
 class HooksController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :basic_auth
+
   def jawbone
     params.tapp
     head(:ok)
