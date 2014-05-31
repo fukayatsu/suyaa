09fdea7dcd594a8f6b9aa5724a068f1ce25a44c8
fukayatsu <fukayatsu@gmail.com>
2014-05-31 23:52:15 +0900

    log hook event

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,7 +3,22 @@ class HooksController < ApplicationController
   skip_before_action :basic_auth
 
   def jawbone
-    params.tapp
+    events = params[:events]
+    head(:ok) and return unless events
+
+    events.each do |event|
+      case event[:type]
+      when 'mood'
+        puts "mood changed."
+      when 'enter_sleep_mode'
+        puts "( ˘ω˘)"
+      when 'exit_sleep_mode'
+        puts '(\( ⁰⊖⁰)/)'
+      else
+        puts 'unknown event'
+        p event
+      end
+    end
     head(:ok)
   end
 end
