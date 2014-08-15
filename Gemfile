d66a8224ddb9ac49ca86e42f24591fec45d35ea4
fukayatsu <fukayatsu@gmail.com>
2014-08-16 08:48:28 +0900

    Integrate with home electronics

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.1.2'
 
-gem 'rails', '4.1.1'
+gem 'rails', '4.1.4'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -20,6 +20,7 @@ gem 'omniauth-twitter'
 
 # external services
 gem 'twitter'
+gem 'aws-sdk'
 
 # settings
 gem 'figaro'
--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -7,12 +7,12 @@ class HooksController < ApplicationController
     head(:ok) and return unless events
 
     events.each do |event|
-      case event[:type]
-      when 'mood'
-        tweet_with_time 'ふぃー'
+      case event[:action]
       when 'enter_sleep_mode'
+        sqs.send_message('lights:off')
         tweet_with_time '( ˘ω˘)'
       when 'exit_sleep_mode'
+        sqs.send_message('lights:max')
         tweet_with_time '"(\( ⁰⊖⁰)/)"'
       else
         puts 'unknown event'
@@ -23,6 +23,14 @@ class HooksController < ApplicationController
   end
 
 private
+
+  def sqs
+    @sqs ||= AWS::SQS.new(
+      access_key_id:     ENV['AWS_ID'],
+      secret_access_key: ENV['AWS_SECRET'],
+      region:            ENV['AWS_REGION']
+    ).queues[ENV['SQS_URL']]
+  end
 
   def tweet_with_time(text)
     TWITTER.update("@tos #{text} \n\n[#{Time.now}]")
